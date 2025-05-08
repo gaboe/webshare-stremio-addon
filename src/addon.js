3c6bfe2e1dd20586468d28b6cb699a389ec904bf
Gabriel Ecegi <gabrielecegi@gmail.com>
2025-05-08 18:23:00 +0200

    Update addon manifest for nicshare.cz
    Changed the addon ID and name from "Webshare.cz" to "nicshare.cz" to reflect the new branding. Updated the configuration titles for login and password to match the new service. Ensured consistent formatting and improved readability in the manifest structure.

--- src/addon.js
+++ src/addon.js
@@ -1,52 +1,47 @@
-const { addonBuilder } = require("stremio-addon-sdk")
-const needle = require('needle')
-const webshare = require('./webshare')
-const { findShowInfo } = require("./meta")
+const { addonBuilder } = require("stremio-addon-sdk");
+const needle = require("needle");
+const webshare = require("./webshare");
+const { findShowInfo } = require("./meta");
 
 // Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/manifest.md
 const manifest = {
-	"id": "community.coffei.webshare",
-	"version": "0.2.0",
-	"catalogs": [],
-	"resources": ["stream"],
-	"types": [
-		"movie",
-		"series"
-	],
-	"name": "Webshare.cz",
-	"description": "Simple webshare.cz search and streaming.",
-	"idPrefixes": [
-		"tt"
-	],
-	"behaviorHints": { "configurable": true, "configurationRequired": true },
-	"config": [
-		{
-			"key": "login",
-			"type": "text",
-			"title": "Webshare.cz login - username or email",
-			"required": true
-		},
-		{
-			"key": "password",
-			"type": "password",
-			"title": "Webshare.cz password",
-			"required": true
-		}
-	]
-}
-const builder = new addonBuilder(manifest)
+  id: "community.gabecko.webshare",
+  version: "0.2.0",
+  catalogs: [],
+  resources: ["stream"],
+  types: ["movie", "series"],
+  name: "nicshare.cz",
+  description: "Simple nicshare.cz search and streaming.",
+  idPrefixes: ["tt"],
+  behaviorHints: { configurable: true, configurationRequired: true },
+  config: [
+    {
+      key: "login",
+      type: "text",
+      title: "nicshare.cz login - username or email",
+      required: true,
+    },
+    {
+      key: "password",
+      type: "password",
+      title: "nicshare.cz password",
+      required: true,
+    },
+  ],
+};
+const builder = new addonBuilder(manifest);
 
 builder.defineStreamHandler(async function (args) {
-	const info = await findShowInfo(args.type, args.id)
-	if (info) {
-		const config = args.config || {}
-		const wsToken = await webshare.login(config.login, config.password)
-		const streams = await webshare.search(info, wsToken)
-		const streamsWithUrl = await webshare.addUrlToStreams(streams, wsToken)
+  const info = await findShowInfo(args.type, args.id);
+  if (info) {
+    const config = args.config || {};
+    const wsToken = await webshare.login(config.login, config.password);
+    const streams = await webshare.search(info, wsToken);
+    const streamsWithUrl = await webshare.addUrlToStreams(streams, wsToken);
 
-		return { streams: streamsWithUrl }
-	}
-	return { streams: [] }
-})
+    return { streams: streamsWithUrl };
+  }
+  return { streams: [] };
+});
 
-module.exports = builder.getInterface()
+module.exports = builder.getInterface();
